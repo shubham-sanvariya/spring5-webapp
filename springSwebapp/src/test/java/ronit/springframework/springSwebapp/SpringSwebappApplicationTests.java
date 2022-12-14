88aebf0b3ed8afb5a4fa52fcd8cf03a565742ac4
shubham-sanvariya <shubhamsanvariyait.com>
2022-12-14 22:23:16 +0530

    Initial commit

---
+++ springSwebapp/src/test/java/ronit/springframework/springSwebapp/SpringSwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package ronit.springframework.springSwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
